c02bd834617da1e9c2b3af065504bf7730ee0d7c
Erdem <erdemaslan1003@gmail.com>
2025-03-16 19:42:44 +0300

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,27 +1,15 @@
 function toggleLanguage() {
-    localStorage.setItem("language", "en");
-    updateLanguage();
-}
+    let currentLang = document.getElementById("language-icon").alt;
 
-function toggleLanguageTR() {
-    localStorage.setItem("language", "tr");
-    updateLanguage();
-}
-
-function updateLanguage() {
-    const language = localStorage.getItem("language") || "tr"; // Varsayılan olarak Türkçe
-    if (language === "en") {
+    if (currentLang === "English") {
         document.getElementById("title").innerText = "Score Game";
         document.getElementById("subtitle").innerText = "Play the best games here!";
         document.getElementById("game-list-title").innerText = "Game List";
-        document.getElementById("qr-title").innerText = "Open the Site on Your Phone";
-        document.getElementById("qr-text").innerText = "Scan the QR code for quick access!";
+        document.getElementById("qr-title").innerText = "Open on Your Phone";
+        document.getElementById("qr-text").innerText = "Scan the QR code for easy access on mobile!";
         document.getElementById("footer-text").innerText = "© 2025 Score Game. All rights reserved.";
-
-        // Bayrak değiştir
-        document.getElementById("language-icon").style.display = "none";
-        document.getElementById("language-icon-tr").style.display = "inline-block";
-
+        document.getElementById("language-icon").src = "assets/tr-flag.png";
+        document.getElementById("language-icon").alt = "Türkçe";
     } else {
         document.getElementById("title").innerText = "Skor Oyun";
         document.getElementById("subtitle").innerText = "En iyi oyunları burada oyna!";
@@ -29,12 +17,7 @@ function updateLanguage() {
         document.getElementById("qr-title").innerText = "Siteyi Telefonundan Aç";
         document.getElementById("qr-text").innerText = "Mobil cihazından kolayca erişmek için QR kodu tara!";
         document.getElementById("footer-text").innerText = "© 2025 Skor Oyun. Tüm hakları saklıdır.";
-
-        // Bayrak değiştir
-        document.getElementById("language-icon").style.display = "inline-block";
-        document.getElementById("language-icon-tr").style.display = "none";
+        document.getElementById("language-icon").src = "assets/usa-flag.png";
+        document.getElementById("language-icon").alt = "English";
     }
 }
-
-// Sayfa yüklendiğinde dili kontrol et
-document.addEventListener("DOMContentLoaded", updateLanguage);
