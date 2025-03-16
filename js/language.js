443a55f77e8cb40024eadea247de80390485e6f2
Erdem <erdemaslan1003@gmail.com>
2025-03-16 19:56:34 +0300

    fix

--- js/language.js
+++ js/language.js
@@ -1,18 +1,14 @@
 function toggleLanguage() {
-    let currentLang = document.getElementById("language-icon").alt;
+    document.getElementById("title").innerText = "Score Game";
+    document.getElementById("subtitle").innerText = "Play the best games here!";
+    document.getElementById("game-list-title").innerText = "Game List";
+    document.getElementById("qr-title").innerText = "Open on Your Phone";
+    document.getElementById("qr-text").innerText = "Scan the QR code for easy access on mobile!";
+    document.getElementById("footer-text").innerText = "© 2025 Score Game. All rights reserved.";
 
-    if (currentLang === "English") {
-        document.getElementById("title").innerText = "Score Game";
-        document.getElementById("subtitle").innerText = "Play the best games here!";
-        document.getElementById("game-list-title").innerText = "Game List";
-        document.getElementById("qr-title").innerText = "Open on Your Phone";
-        document.getElementById("qr-text").innerText = "Scan the QR code for easy access on mobile!";
-        document.getElementById("footer-text").innerText = "© 2025 Score Game. All rights reserved.";
-        
-        // Bayrakları değiştir
-        document.getElementById("language-icon").style.display = "none";
-        document.getElementById("language-icon-tr").style.display = "inline-block";
-    }
+    document.getElementById("language-icon").style.display = "none";
+    document.getElementById("language-icon-tr").style.display = "inline-block";
+}
 }
 
 function toggleLanguageTR() {
@@ -23,7 +19,6 @@ function toggleLanguageTR() {
     document.getElementById("qr-text").innerText = "Mobil cihazından kolayca erişmek için QR kodu tara!";
     document.getElementById("footer-text").innerText = "© 2025 Skor Oyun. Tüm hakları saklıdır.";
 
-    // Bayrakları değiştir
-    document.getElementById("language-icon").style.display = "inline-block";
     document.getElementById("language-icon-tr").style.display = "none";
+    document.getElementById("language-icon").style.display = "inline-block";
 }
